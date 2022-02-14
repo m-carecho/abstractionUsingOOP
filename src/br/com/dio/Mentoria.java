19a9b99275af5ced598c38d3da923f63850d4138
m-carecho <milena.c@aluno.ifsp.edu.br>
2022-02-14 18:29:11 -0300

    create class

---
+++ src/br/com/dio/Mentoria.java
@@ -0,0 +1,34 @@
+package br.com.dio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+    private LocalDate date;
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20;
+    }
+
+    public Mentoria() {
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+
+}
